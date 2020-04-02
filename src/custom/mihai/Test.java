5c3b9f5ed3ae1a063603dfefe5572ec138c4a7dc
mihai.ghidoveanu <mihai.ghidoveanu@gmail.com>
2020-04-02 18:40:54 +0200

    add L to log title

--- src/custom/mihai/Test.java
+++ src/custom/mihai/Test.java
@@ -13,6 +13,7 @@ import java.io.IOException;
  * Created with IntelliJ IDEA. User: Diego Date: 04/10/13 Time: 16:29 This is a
  * Java port from Tom Schaul's VGDL - https://github.com/schaul/py-vgdl
  */
+
 public class Test {
 
     public static void main(String[] args) throws IOException {
@@ -39,12 +40,13 @@ public class Test {
         //Actions to file optional (set saveActions to true). 
         //Logs to output/{experimentName}.
 
-        String experimentName = "test";
-        String timeStamp = new SimpleDateFormat("MM.dd.HH.mm").format(new java.util.Date());
-        String logFileName = "logs/" + experimentName + "_" + timeStamp + ".csv";
-        ScoreLogger logger = new ScoreLogger(logFileName);
         int[] gamesIdx = new int[]{0};
         int N = gamesIdx.length, L = 1, M = 1;
+        String experimentName = "test";
+        String timeStamp = new SimpleDateFormat("MM.dd.HH.mm").format(new java.util.Date());
+        String logFileName = "logs/" + experimentName + "_" + timeStamp + "_" + String.valueOf(L) + ".csv";
+
+        ScoreLogger logger = new ScoreLogger(logFileName);
 		boolean saveActions = false;
 		String[] levels = new String[L];
 		String[] actionFiles = new String[L*M];
